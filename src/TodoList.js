29c955beb8a8867b7e6eab0251171049db4c1485
Cooper <37494012+CooperM93@users.noreply.github.com>
2019-12-23 18:25:22 -0800

    Set up local storage hook to save lists

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,23 +5,24 @@ import Divider from '@material-ui/core/Divider'
 import Todo from './Todo';
 
 export default function TodoList({ todos, removeTodo, toggleTodo, editTodo }) {
-    return (
-        <Paper>
-            <List>
-                {todos.map((todo, i) => (
-                    <>
-                        <Todo 
-                            task={todo.task} 
-                            key={todo.id} 
-                            id={todo.id} 
-                            completed={todo.completed} 
-                            toggleTodo={toggleTodo} 
-                            removeTodo={removeTodo} 
-                            editTodo={editTodo} />
-                        {i < todos.length - 1 && <Divider />}
-                    </>
-                ))}
-            </List>
-        </Paper>
-    )
+    if (todos.length) {
+        return (
+            <Paper>
+                <List>
+                    {todos.map((todo, i) => (
+                        <>
+                            <Todo 
+                                {...todo}
+                                key={todo.id} 
+                                toggleTodo={toggleTodo} 
+                                removeTodo={removeTodo} 
+                                editTodo={editTodo} />
+                            {i < todos.length - 1 && <Divider />}
+                        </>
+                    ))}
+                </List>
+            </Paper>
+        )
+    };
+    return null
 }
---
+++ src/hooks/useTodoState.js
@@ -0,0 +1,35 @@
+import useLocalStorageState from './useLocalStorageState'
+import uuid from 'uuid/v4'
+
+export default initialTodos => {
+    const [todos, setTodos] = useLocalStorageState("todos", initialTodos);
+    const addTodo = newTodoText => {
+        setTodos([...todos, {id: uuid(), task: newTodoText, completed: false}])
+    }
+    const removeTodo = todoId => {
+        const updatedTodos = todos.filter(todo => todo.id !== todoId);
+        setTodos(updatedTodos)
+    }
+    const toggleTodo = todoId => {
+        const updatedTodos = todos.map(todo =>
+             todo.id === todoId ? {...todo, completed: !todo.completed} : todo
+        );
+        setTodos(updatedTodos);
+    }
+    const editTodo = (todoId, newTask) => {
+        const updatedTodos = todos.map(todo =>
+            todo.id === todoId ? { ...todo, task: newTask } : todo
+        );
+        setTodos(updatedTodos);
+    }
+    return {
+        todos,
+        addTodo,
+        removeTodo,
+        editTodo,
+        toggleTodo,
+    }
+}
+
+
+
